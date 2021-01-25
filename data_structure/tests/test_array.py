ad842f562e0ba53487209c514e930226e2366022
gary.cheung <gary.cheung@ticketmaster.com>
2021-01-24 19:22:13 -0800

    updated method and test case

--- data_structure/tests/test_array.py
+++ data_structure/tests/test_array.py
@@ -3,17 +3,36 @@ from data_structure.array import PlusOne
 
 
 class TestPlusOne(unittest.TestCase):
-    def test_plusOne_one(self):
-        output = PlusOne().plusOne([1, 2, 9])
-        ans = [1, 3, 0]
-        self.assertEqual(ans, output)
+    """
+    test the plusOne() method
+    """
 
-    def test_plusOne_all_carry_over(self):
-        output = PlusOne().plusOne([9, 9])
-        ans = [1, 0, 0]
-        self.assertEqual(ans, output)
-
-    def test_plusOne_two(self):
+    def test_one_digit_no_carryover(self):
         output = PlusOne().plusOne([0])
-        ans = [1]
-        self.assertEqual(ans, output)
+        expected_output = [1]
+        self.assertEqual(output, expected_output)
+
+    def test_two_digits_no_carryover(self):
+        output = PlusOne().plusOne([2, 2])
+        expected_output = [2, 3]
+        self.assertEqual(output, expected_output)
+
+    def test_many_digits_no_carryover(self):
+        output = PlusOne().plusOne([1, 2, 3, 4, 5, 6, 7, 8])
+        expected_output = [1, 2, 3, 4, 5, 6, 7, 9]
+        self.assertEqual(output, expected_output)
+
+    def test_1_carryover_with_no_added_length(self):
+        output = PlusOne().plusOne([1, 3, 9])
+        expected_output = [1, 4, 0]
+        self.assertEqual(output, expected_output)
+
+    def test_multiple_carryovers_with_no_added_length(self):
+        output = PlusOne().plusOne([1, 9, 9, 9, 9])
+        expected_output = [2, 0, 0, 0, 0]
+        self.assertEqual(output, expected_output)
+
+    def test_multiple_carryovers_with_added_length(self):
+        output = PlusOne().plusOne([9, 9, 9])
+        expected_output = [1, 0, 0, 0]
+        self.assertEqual(output, expected_output)
